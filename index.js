b91ea3c582629b0f2abddb384c5ba1e67b4b3737
stuhan64 <stuhan64@gmail.com>
2025-07-07 14:36:37 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+{
+  "name": "astroapp-shopify-backend",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "axios": "^1.6.0",
+    "express": "^4.18.2",
+    "form-data": "^4.0.0",
+    "multer": "^1.4.4",
+    "fs": "0.0.1-security",
+    "path": "^0.12.7"
+  }
+}
