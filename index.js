54ce86225e155a1dda8727944b5fc3d4aa2278cf
stuhan64 <stuhan64@gmail.com>
2026-05-06 19:10:41 -0700

    Decrease gap constants for layout
    Reduced gap constants from 8 to 2 for layout adjustments.

--- index.js
+++ index.js
@@ -525,10 +525,10 @@ app.post('/upload-design', async (req, res) => {
       const smallSize  = 280;
       const lineW      = sunSize;
       const lineH      = 18;   // Bold lines for print
-      const gapA       = 8;
-      const gapB       = 8;
-      const gapC       = 8;
-      const gapD       = 8;
+      const gapA       = 2;
+      const gapB       = 2;
+      const gapC       = 2;
+      const gapD       = 2;
       const canvasW    = sunSize + 80;
       const canvasH    = 20 + smallSize + gapA + lineH + gapB + sunSize + gapC + lineH + gapD + smallSize + 40;
 
